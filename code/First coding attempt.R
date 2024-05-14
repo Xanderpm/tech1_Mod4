0575a12900ca2a4dfc60e00f67324ac4629f8d9e
Xanderpm <xander.p.maness@gmail.com>
2024-05-15 09:55:39 +1000

    updated code

--- code/First coding attempt.R
+++ code/First coding attempt.R
@@ -1,1 +1,11 @@
 #this is code
+
+#install.packages("usethis")
+credentials::git_credential_ask()
+usethis::git_sitrep()
+
+# Creating github token
+usethis::create_github_token()
+
+# Entering token as credential 
+gitcreds::gitcreds_set()
